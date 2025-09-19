2e9d90e4868e7cf4df9805238e83c554f4989015
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-09-19 15:57:52 -0300

    Aula 40 - inferencia de tipos

---
+++ Seção 10 - TypeScript e Orientação a Objetos/Aula 40 - Inferencia de tipos/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let mensagem = 'Seja bem vindo!!!';
+let temporadasFriends = 10;
+let estudandoAngular = true;
+let listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+let listaDeFrutas2 = ['Uva', 'Banana', 'Abacaxi'];
+let notasDasProvas = [7.5, 8, 9];
+let notasDasProvas2 = [7.5, 8, 9];
+//# sourceMappingURL=app.js.map
