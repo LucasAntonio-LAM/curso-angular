9d8f5bcccdb9ed5bb87d7303876890ca2cd1b95b
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-09-19 15:53:44 -0300

    Entendendo o compilador e config.json

---
+++ Seção 10 - TypeScript e Orientação a Objetos/Aula 39 - Entendendo o compilador e configuração json/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ola = (nome, sobrenome) => {
+    console.log(`Olá ${nome} ${sobrenome}`);
+};
+ola('Lucas', 'Antonio');
+//# sourceMappingURL=app.js.map
