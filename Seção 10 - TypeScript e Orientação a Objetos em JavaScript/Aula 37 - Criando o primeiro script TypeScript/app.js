2a6adc56f385461d81379d9f25733a35a5cd3581
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-09-19 15:27:10 -0300

    arquivo de config json e -w para atualizar automaticamente o transpile

---
+++ Seção 10 - TypeScript e Orientação a Objetos em JavaScript/Aula 37 - Criando o primeiro script TypeScript/app.js
@@ -0,0 +1,4 @@
+let ola = function (nome) {
+    console.log("Ol\u00E1 ".concat(nome));
+};
+ola("Jorge");
---
+++ Seção 10 - TypeScript e Orientação a Objetos em JavaScript/Aula 37 - Criando o primeiro script TypeScript/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let ola = (nome, sobrenome) => {
+    console.log(`Olá ${nome} ${sobrenome}`);
+};
+ola("Jorge", "Antonio");
+//# sourceMappingURL=app.js.map
