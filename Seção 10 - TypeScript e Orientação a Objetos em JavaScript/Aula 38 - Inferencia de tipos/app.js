8d68113b3fb23289fa77762087dc7f6ca791c567
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-09-19 15:40:17 -0300

    inferencia de tipos

---
+++ Seção 10 - TypeScript e Orientação a Objetos em JavaScript/Aula 38 - Inferencia de tipos/app.js
@@ -0,0 +1,5 @@
+var mensagem = 'Seja bem vindo!!!';
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+var notasDeProvas = [7.5, 8, 9];
---
+++ Seção 10 - TypeScript e Orientação a Objetos em JavaScript/Aula 38 - Inferencia de tipos/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let mensagem = 'Seja bem vindo!!!';
+let temporadasFriends = 10;
+let estudandoAngular = true;
+let listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+let notasDeProvas = [7.5, 8, 9];
+//# sourceMappingURL=app.js.map
