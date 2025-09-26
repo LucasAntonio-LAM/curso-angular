861283663110d373d7be19f46338e7a82736d88a
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-09-26 14:47:55 -0300

    Componente Painel

--- Seção 11 - Introdução ao Angular 4/Aula 51 - Criando o primeiro APP Angular/app1/src/app/app.ts
+++ Seção 11 - Introdução ao Angular 4/Aula 51 - Criando o primeiro APP Angular/app1/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopoComponent } from './topo/topo.component';
+import { Painel } from './painel/painel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TopoComponent
+    TopoComponent,
+    Painel
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ Seção 11 - Introdução ao Angular 4/Aula 51 - Criando o primeiro APP Angular/app1/src/app/painel/painel.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel',
+  standalone: true,
+  imports: [],
+  templateUrl: './painel.html',
+  styleUrl: './painel.css'
+})
+export class Painel {
+
+}
