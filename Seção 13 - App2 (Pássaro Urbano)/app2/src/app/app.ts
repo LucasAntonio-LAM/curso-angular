d9dc5666d33c935bc027c00ea427f61768e7a937
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-07 15:15:15 -0300

    Arquivos necessarios

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Topo } from './topo/topo';
+import { Rodape } from './rodape/rodape';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Topo, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
