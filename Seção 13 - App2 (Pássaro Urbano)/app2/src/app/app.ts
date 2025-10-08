5fae0b35e4c9fbcbd1dc515fd10fd9f9b55a417e
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-08 14:43:36 -0300

    Criando arquivo service.ts

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { Topo } from './topo/topo';
 import { Rodape } from './rodape/rodape';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Topo, Rodape],
+  imports: [Home, Topo, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
