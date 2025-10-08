49084c9ec88b0680e49f4b47bad9e3765edb8406
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-08 14:55:45 -0300

    Classes de serviços e instanciar no modulo

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/home/home.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { OfertasService } from '../ofertas.service';
 @Component({
   selector: 'app-home',
   imports: [],
+  providers: [OfertasService],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  constructor(private ofertasService: OfertasService) {
 
+  }
+
+  ngOnInit() {
+    console.log(this.ofertasService.getOfertas())
+  }
 }
