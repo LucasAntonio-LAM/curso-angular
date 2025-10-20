e3b6310b673f48f2537334af82cf39e59d098896
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-20 15:19:56 -0300

    Injetando o serviço HTTP

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
 import { Home } from './home/home';
 import { Topo } from './topo/topo';
 import { Rodape } from './rodape/rodape';
 
 @Component({
+  providers: [HttpClient],
   selector: 'app-root',
   imports: [Home, Topo, Rodape],
   templateUrl: './app.html',
