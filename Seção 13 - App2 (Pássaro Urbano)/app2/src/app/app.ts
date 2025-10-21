753303d55b459202b3e22fc28ec329365eebfde8
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-21 15:01:24 -0300

    Criação do restaurantesComponent e DiversaoComponent

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.ts
@@ -6,11 +6,13 @@ import { HttpClient } from '@angular/common/http';
 import { Home } from './home/home';
 import { Topo } from './topo/topo';
 import { Rodape } from './rodape/rodape';
+import { RestaurantesComponent } from './restaurantes-component/restaurantes-component';
+import { DiversaoComponent } from './diversao-component/diversao-component';
 
 @Component({
   providers: [HttpClient],
   selector: 'app-root',
-  imports: [Home, Topo, Rodape],
+  imports: [Home, Topo, Rodape, RestaurantesComponent, DiversaoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/restaurantes-component/restaurantes-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantes',
+  imports: [],
+  templateUrl: './restaurantes-component.html',
+  styleUrl: './restaurantes-component.css'
+})
+export class RestaurantesComponent {
+
+}
