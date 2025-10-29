74437159b618dde534f78836cfef291507f749ec
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-29 15:15:07 -0300

    Recuperando parametros da rota ativa

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-oferta-component',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './oferta-component.html',
   styleUrl: './oferta-component.css'
 })
 export class OfertaComponent {
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    console.log('ID recuperado na rota:', this.route.snapshot.params['id'])
+  }
 }
