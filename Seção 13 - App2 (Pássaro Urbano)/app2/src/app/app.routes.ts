a0e72c39b61a3136040ef798f8001577844871bc
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-29 14:31:09 -0300

    Criando e navegando pelo ofertaComponent

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.routes.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.routes.ts
@@ -5,11 +5,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { RestaurantesComponent } from './restaurantes-component/restaurantes-component';
 import { DiversaoComponent } from './diversao-component/diversao-component';
-
+import { OfertaComponent } from './oferta-component/oferta-component';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'restaurantes', component: RestaurantesComponent},
-    {path: 'diversao', component: DiversaoComponent}
+    {path: 'diversao', component: DiversaoComponent},
+    {path: 'oferta', component: OfertaComponent}
 ];
 
---
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oferta-component',
+  imports: [],
+  templateUrl: './oferta-component.html',
+  styleUrl: './oferta-component.css'
+})
+export class OfertaComponent {
+
+}
