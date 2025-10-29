f9efb4cb1498cf5048118b8a5bbe63ec41e02e17
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-10-29 15:25:32 -0300

    recuperando parametros da rota com subscribe

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
@@ -14,5 +14,8 @@ export class OfertaComponent {
 
   ngOnInit() {
     console.log('ID recuperado na rota:', this.route.snapshot.params['id'])
+    // this.route.params.subscribe((parametro) => {
+    //   console.log(parametro, id)
+    // })
   }
 }
