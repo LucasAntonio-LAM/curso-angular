c55d342525c809c500594f13e48361325da8a75e
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-11-17 20:25:14 -0300

    Configurando rotas filhas

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.routes.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/app.routes.ts
@@ -6,12 +6,20 @@ import { Home } from './home/home';
 import { RestaurantesComponent } from './restaurantes-component/restaurantes-component';
 import { DiversaoComponent } from './diversao-component/diversao-component';
 import { OfertaComponent } from './oferta-component/oferta-component';
+import { ComoUsar } from './oferta-component/como-usar/como-usar';
+import { OndeFica } from './oferta-component/onde-fica/onde-fica';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'restaurantes', component: RestaurantesComponent},
     {path: 'diversao', component: DiversaoComponent},
     {path: 'oferta', component: Home},
-    {path: 'oferta/:id', component: OfertaComponent}
+    {path: 'oferta/:id', component: OfertaComponent, 
+        children: [
+            {path: '', component: ComoUsar},
+            {path: 'como-usar', component: ComoUsar},
+            {path: 'onde-fica', component: OndeFica}
+        ]
+    }
 ];
 
