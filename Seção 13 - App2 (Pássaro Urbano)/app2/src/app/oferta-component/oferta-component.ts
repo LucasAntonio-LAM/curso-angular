9406d55480031c25722cfc9840236a029238696e
LucasAntonio-LAM <lucasantonions45@gmail.com>
2025-11-17 19:33:53 -0300

    Processamento assincrono e a construção do template

--- Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
+++ Seção 13 - App2 (Pássaro Urbano)/app2/src/app/oferta-component/oferta-component.ts
@@ -25,6 +25,7 @@ export class OfertaComponent {
       .then((oferta: Oferta) => {
         this.oferta = oferta
       })
+      
     //console.log('ID recuperado na rota:', this.route.snapshot.params['id'])
     // this.route.params.subscribe((parametro) => {
     //   console.log(parametro, id)
